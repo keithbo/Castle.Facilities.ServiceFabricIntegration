885c6e0b7c27fe02f719c41afc9e402fad817c4a
keithbo <keith.bosacker@gmail.com>
2017-06-18 22:18:33 -0500

    Maintenance update to SF 2.6.210
    This update is simply a routine version increment for Service Fabric SDK
    2.6.210 May 2017 update

--- Castle.Facilities.ServiceFabricIntegration.Actors/Properties/AssemblyInfo.cs
+++ Castle.Facilities.ServiceFabricIntegration.Actors/Properties/AssemblyInfo.cs
@@ -9,17 +9,17 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Castle.Facilities.ServiceFabricIntegration.Actors")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4efb20d0-db91-4bc8-b140-ef8dd1404a53")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.5.0-alpha")]
+[assembly: AssemblyInformationalVersion("1.6.0-alpha")]
 [assembly: AssemblyConfiguration("debug")]
 #else
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
 [assembly: AssemblyConfiguration("retail")]
 #endif
--- Castle.Facilities.ServiceFabricIntegration/Properties/AssemblyInfo.cs
+++ Castle.Facilities.ServiceFabricIntegration/Properties/AssemblyInfo.cs
@@ -8,18 +8,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Castle.Facilities.ServiceFabricIntegration")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("94d42e34-b312-4dc7-b077-21d082e18bbc")]
-[assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyVersion("1.6.0")]
+[assembly: AssemblyFileVersion("1.6.0")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("1.5.0-alpha")]
+[assembly: AssemblyInformationalVersion("1.6.0-alpha")]
 [assembly: AssemblyConfiguration("debug")]
 #else
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("1.6.0")]
 [assembly: AssemblyConfiguration("retail")]
 #endif
