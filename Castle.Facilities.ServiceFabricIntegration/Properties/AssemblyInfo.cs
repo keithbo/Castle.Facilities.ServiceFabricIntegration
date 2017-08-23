41d6998584782c58e563f45800dea617ed1f475b
keithbo <keith.bosacker@gmail.com>
2017-08-23 08:11:21 -0500

    Update project structure to vs 2017
    Change csproj to new csproj structure.
    Bump Service Fabric to 2.7.198
    Bump Castle Windsor to 4.0

--- Castle.Facilities.ServiceFabricIntegration/Properties/AssemblyInfo.cs
+++ Castle.Facilities.ServiceFabricIntegration/Properties/AssemblyInfo.cs
@@ -1,25 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Castle.Facilities.ServiceFabricIntegration")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Castle.Facilities.ServiceFabricIntegration")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("94d42e34-b312-4dc7-b077-21d082e18bbc")]
-[assembly: AssemblyVersion("1.6.0")]
-[assembly: AssemblyFileVersion("1.6.0")]
-#if DEBUG
-[assembly: AssemblyInformationalVersion("1.6.0-alpha")]
-[assembly: AssemblyConfiguration("debug")]
-#else
-[assembly: AssemblyInformationalVersion("1.6.0")]
-[assembly: AssemblyConfiguration("retail")]
-#endif
